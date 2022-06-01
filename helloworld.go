890ffbc02ee986ec7df15482a30b568bee302cbe
Lei Wang <tendant@gmail.com>
2022-06-01 13:48:32 -0700

    create event

--- helloworld.go
+++ helloworld.go
@@ -1,19 +1,42 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	cloudevents "github.com/cloudevents/sdk-go/v2"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("helloworld: received a request")
 	target := os.Getenv("TARGET")
+	brokerUrl := os.Getenv("BROKER_URL")
 	if target == "" {
 		target = "World"
 	}
 	fmt.Fprintf(w, "Hello %s!\n", target)
+
+	c, err := cloudevents.NewClientHTTP()
+	if err != nil {
+		log.Fatalf("failed to create client, %v", err)
+	}
+
+	// Create an Event.
+	event := cloudevents.NewEvent()
+	event.SetSource("example/uri")
+	event.SetType("example.type")
+	event.SetData(cloudevents.ApplicationJSON, map[string]string{"hello": "world"})
+
+	// Set a target.
+	ctx := cloudevents.ContextWithTarget(context.Background(), brokerUrl)
+
+	// Send that Event.
+	if result := c.Send(ctx, event); cloudevents.IsUndelivered(result) {
+		log.Fatalf("failed to send, %v", result)
+	}
 }
 
 func main() {
